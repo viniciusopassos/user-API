70fd57c89480c3e485ef0e42e643ac9a398b6292
viniciusOP <viniciusopassos@hotmail.com>
2022-12-27 18:43:43 -0300

    feat: injeção de dependências por construtor

---
+++ src/User-Api.Web/Repository/UserRepository.cs
@@ -0,0 +1,11 @@
+namespace User_Api.Web.Repository
+{
+    public class UserRepository
+    {
+        private readonly UserContext _context;
+        public UserRepository(UserContext context)
+        {
+            _context = context;
+        }
+    }
+}
